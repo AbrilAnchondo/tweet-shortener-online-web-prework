0dc90f91992784793ef892ec83520a2d075e1eb7
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-15 05:23:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).size > 140 
+  if word_substituter(tweet).char > 140 
     puts word_substituter(tweet)[0..136] +  "..."
   else
     tweet
